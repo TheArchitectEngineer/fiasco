c35af091a8b5ede57f2ee9fcd0c568a8826ac169
Frank Mehnert <frank.mehnert@kernkonzept.com>
2026-09-07 00:00:00 +0000

    disasm: fixes for RISC-V from the upstream 'v5' branch
    Change-Id: I3d45710f3fe2125e51df58c6d7d18c1eb7c6484c
    Signed-off-by: Frank Mehnert <frank.mehnert@kernkonzept.com>

--- src/lib/disasm/capstone/include/capstone/riscv.h
+++ src/lib/disasm/capstone/include/capstone/riscv.h
@@ -481,25 +481,25 @@ typedef enum riscv_insn {
 
 //> Group of RISCV instructions
 typedef enum riscv_insn_group {
-#if 0
-  	{ RISCV_GRP_HASSTDEXTA, 0 },
-  	{ RISCV_GRP_HASSTDEXTA, RISCV_GRP_ISRV64, 0 },
-  	{ RISCV_GRP_HASSTDEXTC, 0 },
-  	{ RISCV_GRP_HASSTDEXTC, RISCV_GRP_HASSTDEXTD, 0 },
-  	{ RISCV_GRP_HASSTDEXTC, RISCV_GRP_HASSTDEXTF, RISCV_GRP_ISRV32, 0 },
-  	{ RISCV_GRP_HASSTDEXTC, RISCV_GRP_ISRV32, 0 },
-  	{ RISCV_GRP_HASSTDEXTC, RISCV_GRP_ISRV64, 0 },
-  	{ RISCV_GRP_HASSTDEXTD, 0 },
-  	{ RISCV_GRP_HASSTDEXTD, RISCV_GRP_ISRV64, 0 },
-  	{ RISCV_GRP_HASSTDEXTF, 0 },
-  	{ RISCV_GRP_HASSTDEXTF, RISCV_GRP_ISRV64, 0 },
-  	{ RISCV_GRP_HASSTDEXTM, 0 },
-  	{ RISCV_GRP_HASSTDEXTM, RISCV_GRP_ISRV64, 0 },
-  	{ RISCV_GRP_ISRV64, 0 },
-#endif
-  	RISCV_GRP_INVALID = 0, // = CS_GRP_INVALID
-  	RISCV_GRP_JUMP,
+  	RISCV_GRP_INVALID = 0, ///< = CS_GRP_INVALID
+
+  	// Generic groups
+  	// all jump instructions (conditional+direct+indirect jumps)
+  	RISCV_GRP_JUMP,	///< = CS_GRP_JUMP
+  	// all call instructions
+  	RISCV_GRP_CALL,	///< = CS_GRP_CALL
+  	// all return instructions
+  	RISCV_GRP_RET,	///< = CS_GRP_RET
+  	// all interrupt instructions (int+syscall)
+  	RISCV_GRP_INT,	///< = CS_GRP_INT
+  	// all interrupt return instructions
+  	RISCV_GRP_IRET,	///< = CS_GRP_IRET
+  	// all privileged instructions
+  	RISCV_GRP_PRIVILEGE,	///< = CS_GRP_PRIVILEGE
+  	// all relative branching instructions
+  	RISCV_GRP_BRANCH_RELATIVE, ///< = CS_GRP_BRANCH_RELATIVE
   
+  	// Architecture-specific groups
   	RISCV_GRP_ISRV32 = 128,
   	RISCV_GRP_ISRV64,
   	RISCV_GRP_HASSTDEXTA,
